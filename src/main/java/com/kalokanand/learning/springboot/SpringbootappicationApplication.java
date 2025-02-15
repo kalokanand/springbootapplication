fc32820d84a607b12d6c2fea58c379bfe4092f8f
KUMAR ALOK ANAND <alok4java@gmail.com>
2025-02-15 19:06:25 +0530

    first commit

---
+++ src/main/java/com/kalokanand/learning/springboot/SpringbootappicationApplication.java
@@ -0,0 +1,13 @@
+package com.kalokanand.learning.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootappicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootappicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalokanand/learning/springboot/SpringbootappicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalokanand.learning.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootappicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
