e10794bc1dd8410012ee71783de25d75cea65671
KUMAR ALOK ANAND <alok4java@gmail.com>
2025-02-16 17:27:13 +0530

    Customizing Bean Nature in Spring | Bean Lifecycle, @PostConstruct & @PreDestroy

--- src/main/java/com/kalokanand/learning/componentscan/ComponentScanBasePackages.java
+++ src/main/java/com/kalokanand/learning/componentscan/ComponentScanBasePackages.java
@@ -1,11 +1,42 @@
 package com.kalokanand.learning.componentscan;
 
+import com.kalokanand.learning.springboot.components.Order;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ComponentScanBasePackages {
+public class ComponentScanBasePackages implements InitializingBean, DisposableBean {
 
-    public ComponentScanBasePackages(){
-        System.out.println("Initializing ComponentScanBasePackages bean");
+    private Order order;
+
+    public ComponentScanBasePackages(@Qualifier("onlineOrder") Order order){
+        System.out.println("Creating a ComponentScanBasePackages bean");
+        this.order = order;
+        System.out.println("Injecting Dependency");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Init setup");
+    }
+
+    @PostConstruct
+    public String init(){
+        System.out.println("Init setup using annotation");
+        return "";
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy setup");
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("destroy setup annotation");
     }
 }
--- src/main/java/com/kalokanand/learning/springboot/SpringbootappicationApplication.java
+++ src/main/java/com/kalokanand/learning/springboot/SpringbootappicationApplication.java
@@ -1,13 +1,21 @@
 package com.kalokanand.learning.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class SpringbootappicationApplication {
+public class SpringbootappicationApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootappicationApplication.class, args);
+		ConfigurableApplicationContext context= SpringApplication.run(SpringbootappicationApplication.class, args);
+		context.close();
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
